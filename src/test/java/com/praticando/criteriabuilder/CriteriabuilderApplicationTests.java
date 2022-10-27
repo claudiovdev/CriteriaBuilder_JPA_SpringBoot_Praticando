67714f27bb326e55f7d088c07c32bbb80cfeaffc
viniciuscfn <c.viniciusmagalhaes@gmail.com>
2022-10-26 22:18:20 -0300

    Adicionando projeto

---
+++ src/test/java/com/praticando/criteriabuilder/CriteriabuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praticando.criteriabuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriteriabuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
